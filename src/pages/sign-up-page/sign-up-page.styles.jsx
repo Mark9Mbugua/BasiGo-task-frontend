d878a723ef22d38e19e21580c09d9f5945ac3d3c
Mbugua <mbuguamark@adalabsafrica.com>
2022-08-13 16:25:38 +0300

    Fix: samll bug

--- src/pages/sign-up-page/sign-up-page.styles.jsx
+++ src/pages/sign-up-page/sign-up-page.styles.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const PageWrapper = styled.form`
+export const PageWrapper = styled.div`
   display: flex;
   justify-content: center;
   /* border: 1px solid red; */
